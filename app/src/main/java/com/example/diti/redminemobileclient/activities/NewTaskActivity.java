57d7a2144c0802bd972046b712d971b968fc8041
dastit <41509710+dastit@users.noreply.github.com>
2018-08-22 14:56:20 +0300

    new task: small refactor

--- app/src/main/java/com/example/diti/redminemobileclient/activities/NewTaskActivity.java
+++ app/src/main/java/com/example/diti/redminemobileclient/activities/NewTaskActivity.java
@@ -1,7 +1,9 @@
 package com.example.diti.redminemobileclient.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.inputmethodservice.InputMethodService;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,6 +21,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -323,6 +326,9 @@ public class NewTaskActivity extends AppCompatActivity
             @Override
             public void onFocusChange(View view, boolean b) {
                 if(b){
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                     mAssignedTo.showDropDown();
                 }else{
                     mAssignedTo.dismissDropDown();
@@ -455,6 +461,9 @@ public class NewTaskActivity extends AppCompatActivity
         mParentIssue.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    checkParentIssueStep();
+                }
                 verticalStepperForm.goToNextStep();
                 return false;
             }
